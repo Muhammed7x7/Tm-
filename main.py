5f90b74248e40244fb0c73f2f13d3d5f1ad16194
MHD-EKN <109427175+Muhammed7x7@users.noreply.github.com>
2026-03-08 16:19:01 +0300

    Update main.py

--- main.py
+++ main.py
@@ -4,40 +4,40 @@ from telegram import Update
 from telegram.ext import ApplicationBuilder, CommandHandler, MessageHandler, ContextTypes, filters
 
 TOKEN = os.getenv("TELEGRAM_TOKEN")
-DEEPSEEK = os.getenv("DEEPSEEK_API_KEY")
+GROQ_API = os.getenv("GROQ_API_KEY")
 OWNER = int(os.getenv("OWNER_ID"))
 
 memory = {}
 
-def deepseek_chat(user_id, prompt):
+def ai_chat(user, prompt):
 
-    history = memory.get(user_id, [])
+    history = memory.get(user, [])
 
     messages = history + [{"role":"user","content":prompt}]
 
-    url = "https://api.deepseek.com/chat/completions"
+    url = "https://api.groq.com/openai/v1/chat/completions"
 
     headers = {
-        "Authorization": f"Bearer {DEEPSEEK}",
+        "Authorization": f"Bearer {GROQ_API}",
         "Content-Type": "application/json"
     }
 
     data = {
-        "model":"deepseek-chat",
+        "model":"llama3-70b-8192",
         "messages":messages
     }
 
     r = requests.post(url, headers=headers, json=data)
 
     if r.status_code != 200:
-        return "API hata"
+        return r.text
 
     cevap = r.json()["choices"][0]["message"]["content"]
 
     history.append({"role":"user","content":prompt})
     history.append({"role":"assistant","content":cevap})
 
-    memory[user_id] = history[-10:]
+    memory[user] = history[-10:]
 
     return cevap
 
@@ -46,8 +46,7 @@ async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
 
     await update.message.reply_text(
         "🤖 AI Bot hazır!\n\n"
-        "Mesaj yaz → cevap vereyim\n"
-        "/reset → hafızayı temizler"
+        "Mesaj yaz bana."
     )
 
 
@@ -72,7 +71,7 @@ async def chat(update: Update, context: ContextTypes.DEFAULT_TYPE):
 
     await update.message.reply_text("🤔 düşünüyorum...")
 
-    cevap = deepseek_chat(user, msg)
+    cevap = ai_chat(user, msg)
 
     if len(cevap) > 4000:
         for i in range(0, len(cevap), 4000):
